ee115976078eb197626621a552bda27be6533409
Adrian Bautista Orozco <abautista@bandp.tech>
2019-03-13 09:23:07 -0500

    Clase de Destructuracion de objetos

--- src/js/home.js
+++ src/js/home.js
@@ -5,20 +5,70 @@ fetch('https://randomuser.me/api/')
 
 (async function load(){
     async function getData(url){
-        const response = await fetch('https://yts.am/api/v2/list_movies.json')
-        const data = await response.json()
-        return data
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
     }
-    
-    const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
-    const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
-    const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
+    const BASE_URL = 'https://yts.am/api/v2/'
+    const $form = document.getElementById('form')
+    const $home = document.getElementById('home')
+    const $modal = document.getElementById('modal')
+    const $overlay = document.getElementById('overlay')
+    const $hideModal = document.getElementById('hide-modal')
 
-    console.log(actionList, dramaList, animationList)
+    const $modalTitle = $modal.querySelector('h1')
+    const $modalImg = $modal.querySelector('img')
+    const $modalDescription = $modal.querySelector('p')
+    const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action');
+    const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama');
+    const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation');
+    const $featuringContainer = document.getElementById('featuring')
 
-    const $actionContainer = document.getElementById('action')
-    const $dramaContainer = document.getElementById('drama')
-    const $animationContainer = document.getElementById('animation')
+    function setAttributes($element, attributes){
+        for(const attribute in attributes){
+            $element.setAttribute(attribute, attributes[attribute])
+        }
+    }
+
+    function featuringTemplate(movie){
+        return (
+            `<div class="featuring">
+                <div class="featuring-image">
+                    <img src="${movie.medium_cover_image}" width="70" height="100" alt="">
+                </div>
+                <div class="featuring-content">
+                    <p class="featuring-title">Pelicula encontrada</p>
+                    <p class="featuring-album">${movie.title}</p>
+                </div>
+          </div>`
+        )
+    }
+
+    $form.addEventListener('submit',async function(e){
+        e.preventDefault()
+        $home.classList.add('search-active')
+        const $loader =  document.createElement('img')
+        setAttributes($loader, {
+            src: 'src/images/loader.gif',
+            height: '50px',
+            width: '50px'
+        })
+        $featuringContainer.append($loader)
+
+        const data = new FormData($form)
+        const {
+            data: {
+                movies
+            }
+        } = await getData(`${BASE_URL}list_movies.json?limit=1&query_term=${data.get('name')}`)
+        
+        const HTMLString = featuringTemplate(movies[0])
+        $featuringContainer.innerHTML = HTMLString
+
+    })
+    const $actionContainer = document.getElementById('action');
+    const $dramaContainer = document.getElementById('drama');
+    const $animationContainer = document.getElementById('animation');
 
     function templateItemsVideo(movie){
         return (
@@ -33,25 +83,41 @@ fetch('https://randomuser.me/api/')
         );
     }
 
-    actionList.data.movies.forEach(movie => {
-        const HTMLString = templateItemsVideo(movie)
-        const html = document.implementation.createHTMLDocument()
-        html.body.innerHTML = HTMLString
-        //debugger
-        //$actionContainer.append(html.body.children[0])
-        $actionContainer.innerHTML += HTMLString
-    });
-    const $featuringContainer = document.getElementById('featuring')
-    const $form = document.getElementById('form')
-    const $home = document.getElementById('home')
+    function createTemplate(HTMLString){
+        const html = document.implementation.createHTMLDocument();
+        html.body.innerHTML = HTMLString;
+        return html.body.children[0];
+    }
 
-    const $modal = document.getElementById('modal')
-    const $overlay = document.getElementById('overlay')
-    const $hideModal = document.getElementById('hide-modal')
+    function addEventClick($element){
+        $element.addEventListener('click', function(){
+            showModal($element)
+        })
+    }
 
-    const $modalTitle = $modal.querySelector('h1')
-    const $modalImg = $modal.querySelector('img')
-    const $modalDescription = $modal.querySelector('p')
+    function showModal($element){
+        $overlay.classList.add('active')
+        $modal.style.animation = 'modalIn .8s forwards'
+    }
 
-    
+    function hideModal(){
+        $overlay.classList.remove('active')
+        $modal.style.animation = 'modalOut .8s forwards'
+    }
+    $hideModal.addEventListener('click', hideModal)
+    function renderMovieList(list,$container){
+        $container.children[0].remove()
+        list.forEach(movie => {
+            const HTMLString = templateItemsVideo(movie);
+            const movieElement = createTemplate(HTMLString);
+            
+            //debugger
+            $container.append(movieElement);
+            addEventClick(movieElement)
+            //$actionContainer.innerHTML += HTMLString
+        });
+    }
+    renderMovieList(actionList.data.movies,$actionContainer)
+    renderMovieList(dramaList.data.movies,$dramaContainer)
+    renderMovieList(animationList.data.movies,$animationContainer)
 })()
